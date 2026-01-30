4d2ab3e5efebc4bdd418a4bdce0c02b850113c17
Gabriel Jose <gabrieljoseotk@gmail.com>
2026-01-29 22:32:11 -0300

    inicio

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POS_ENGENHARIADESOFTWARE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
